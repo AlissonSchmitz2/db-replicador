57c56f34255bb59d88198beb240cd45cde3646aa
Alisson Schmitz <alissonschmitz2@hotmail.com>
2019-05-22 20:51:08 -0300

    Iniciando projeto

---
+++ src/br/com/dbreplicador/view/Main.java
@@ -0,0 +1,61 @@
+package br.com.dbreplicador.view;
+
+import java.awt.Font;
+import java.util.Enumeration;
+
+import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Muda o Layout da janela para o padrão do Windows
+		try {
+			boolean hasWindowLookAndFeel = false;
+
+			for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				// Windows
+				if ("Windows".equals(info.getName())) {
+					hasWindowLookAndFeel = true;
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+
+			// Alternativa para outros sistemas operacionais
+			if (!hasWindowLookAndFeel) {
+				UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+
+				setUIFont(new FontUIResource(Font.SANS_SERIF, Font.PLAIN, 11));
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		}
+
+		java.awt.EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
+
+				new Window().setVisible(true);;
+			}
+		});
+	}
+
+	public static void setUIFont(javax.swing.plaf.FontUIResource f) {
+		Enumeration<Object> keys = UIManager.getDefaults().keys();
+		while (keys.hasMoreElements()) {
+			Object key = keys.nextElement();
+			Object value = UIManager.get(key);
+			if (value instanceof FontUIResource)
+				UIManager.put(key, f);
+		}
+	}
+
+}
---
+++ src/br/com/dbreplicador/view/Window.java
@@ -0,0 +1,203 @@
+package br.com.dbreplicador.view;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JSeparator;
+import javax.swing.WindowConstants;
+
+public class Window extends JFrame {
+	private static final long serialVersionUID = 3283754083146407662L;
+
+	// Janelas do Menu
+
+	private JMenu menuSistema;
+	private JMenu menuCadastros;
+	private JMenu menuProcessos;
+	private JMenu menuAjuda;
+
+	//private JLabel wallpaper;
+
+	private JSeparator separador;
+
+	private JDesktopPane desktop;
+
+	//private Connection CONNECTION;
+
+	public Window() {
+		super();
+
+		desktop = new JDesktopPane();
+		desktop.setDragMode(JDesktopPane.OUTLINE_DRAG_MODE);
+		desktop.setVisible(true);
+		setContentPane(desktop);
+
+		startingWindow();
+
+		// TODO: Wallpaper do sistema
+		
+		// Full screen
+		setExtendedState(Frame.MAXIMIZED_BOTH);
+	}
+
+	private String getDateTime() {
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date date = new Date();
+		return dateFormat.format(date);
+	}
+
+	private void startingWindow() {
+		String dataLogin = getDateTime();
+		this.setTitle("Replicador Banco de dados v1.0.0-betha      " + "Usuário Logado: " + " - Último Login: " + dataLogin);
+		this.setJMenuBar(getWindowMenuBar());
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		this.setBounds(new Rectangle(0, 0, 796, 713));
+		this.setFocusableWindowState(true);
+		getContentPane().setBackground(new Color(247, 247, 247));
+	}
+
+	/*
+	 * MENU DE NAVEGAÇÃO
+	 */
+	private JMenuBar getWindowMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.add(getMenuSistema());
+		menuBar.add(getMenuCadastros());
+		menuBar.add(getMenuProcessos());
+		menuBar.add(getMenuAjuda());
+
+		return menuBar;
+	}
+
+	// Menu Sistema
+	private JMenu getMenuSistema() {
+		separador = new JSeparator();
+		menuSistema = new JMenu("Sistema");
+		menuSistema.setFont(getDefaultFont());
+
+		menuSistema.add(getMenuItemConfig());
+		menuSistema.add(separador);
+		menuSistema.add(getMenuItemSair());
+
+		return menuSistema;
+	}
+
+	private JMenuItem getMenuItemConfig() {
+		JMenuItem menuItem = new JMenuItem("Configurar");
+		menuItem.setFont(getDefaultFont());
+
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO: Configurações
+			}
+		});
+
+		return menuItem;
+	}
+
+	private JMenuItem getMenuItemSair() {
+		JMenuItem menuItem = new JMenuItem("Sair");
+		menuItem.setFont(getDefaultFont());
+
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
+		return menuItem;
+	}
+
+	// Menu Cadastros
+	private JMenu getMenuCadastros() {
+		menuCadastros = new JMenu("Cadastros");
+		menuCadastros.setFont(getDefaultFont());
+
+		//menuCadastros.add(getMenuItem1());
+	
+		return menuCadastros;
+	}
+
+	// Menu Processos
+	private JMenu getMenuProcessos() {
+		menuProcessos = new JMenu("Processos");
+		menuProcessos.setFont(getDefaultFont());
+
+		//menuCadastros.add(getMenuItem1());
+
+		return menuProcessos;
+	}
+
+	
+	// MENU AJUDA
+	private JMenu getMenuAjuda() {
+		menuAjuda = new JMenu("Ajuda");
+		menuAjuda.setFont(getDefaultFont());
+
+		menuAjuda.add(getMenuItemSobre());
+
+		return menuAjuda;
+	}
+
+	private JMenuItem getMenuItemSobre() {
+		JMenuItem menuItem = new JMenuItem("Sobre");
+		menuItem.setFont(getDefaultFont());
+
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Desenvolvimento:\n\nAlisson Schmitz\n"
+						+ "Edvaldo da Rosa\nGiovane Santiago\n" + "Wilian Hendler", "Informação", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+
+		return menuItem;
+	}
+
+	/*
+	private void abrirFrame(AbstractWindowFrame frame) {
+		boolean frameAlreadyExists = false;
+
+		// Percorre todos os frames adicionados
+		for (JInternalFrame addedFrame : desktop.getAllFrames()) {
+
+			// Se o frame adiconado ja estiver
+			if (addedFrame.getClass().toString().equalsIgnoreCase(frame.getClass().toString())) {
+				// Remove janelas duplicadas
+				addedFrame.moveToFront();
+				frameAlreadyExists = true;
+			}
+
+		}
+
+		try {
+			if (!frameAlreadyExists) {
+				desktop.add(frame);
+				frame.moveToFront();
+			}
+
+			frame.setSelected(true);
+			frame.setVisible(true);
+		} catch (PropertyVetoException e) {
+			JOptionPane.showMessageDialog(rootPane, "Houve um erro ao abrir a janela", "", JOptionPane.ERROR_MESSAGE,
+					null);
+		}
+	}
+*/
+
+	private Font getDefaultFont() {
+		return new java.awt.Font("Dialog", java.awt.Font.PLAIN, 12);
+	}
+}
