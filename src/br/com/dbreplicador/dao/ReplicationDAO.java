0887310d958d11668b03288320abc128ce1bf141
Wilian Borges <wilianx7@hotmail.com>
2019-05-23 16:42:48 -0300

    Adição do Model e DAO para tb_replicacao.

---
+++ src/br/com/dbreplicador/dao/ReplicationDAO.java
@@ -0,0 +1,193 @@
+package br.com.dbreplicador.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.dbreplicador.model.ReplicationModel;
+
+public class ReplicationDAO extends AbstractCrudDAO<ReplicationModel> {
+	private static final String TABLE_NAME = "tb_replicacao";
+
+	private String columnId = "codigo_replicacao";
+
+	private String defaultOrderBy = "codigo_replicacao ASC";
+
+	private String[] defaultValuesToInsert = new String[] {
+			"DEFAULT"
+	};
+
+	private String[] columnsToInsert = new String[] {
+			"codigo_replicacao",
+			"data_atual",
+			"usuario",
+			"nome",
+			"endereco",
+			"porta",
+			"database",
+			"tipo_banco",
+			"url"
+	};
+	
+	private String[] columnsToUpdate = new String[] {
+			"data_atual",
+			"usuario",
+			"nome",
+			"endereco",
+			"porta",
+			"database",
+			"tipo_banco",
+			"url"
+	};
+
+	Connection connection;
+
+	public ReplicationDAO(Connection connection) throws SQLException {
+		this.connection = connection;
+
+		this.connection.setAutoCommit(false);
+	}
+
+	@Override
+	public ReplicationModel insert(ReplicationModel model) throws SQLException {
+		String query = getInsertQuery(TABLE_NAME, columnsToInsert, defaultValuesToInsert);
+
+		PreparedStatement pst = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+		pst.clearParameters();
+
+		setParam(pst, 1, model.getCurrentDate());
+		setParam(pst, 2, model.getUser());
+		setParam(pst, 3, model.getName());
+		setParam(pst, 4, model.getAddress());
+		setParam(pst, 5, model.getDoor());
+		setParam(pst, 6, model.getDatabase());
+		setParam(pst, 7, model.getDatebaseType());
+		setParam(pst, 8, model.getUrl());
+
+		int result = pst.executeUpdate();
+		if (result > 0) {
+			connection.commit();
+
+			ResultSet rs = pst.getGeneratedKeys();
+			if (rs.next()) {
+				return model;
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	public boolean update(ReplicationModel model) throws SQLException {
+		String query = getUpdateQuery(TABLE_NAME, columnId, columnsToUpdate);
+
+		PreparedStatement pst = connection.prepareStatement(query);
+
+		setParam(pst, 1, model.getCurrentDate());
+		setParam(pst, 2, model.getUser());
+		setParam(pst, 3, model.getName());
+		setParam(pst, 4, model.getAddress());
+		setParam(pst, 5, model.getDoor());
+		setParam(pst, 6, model.getDatabase());
+		setParam(pst, 7, model.getDatebaseType());
+		setParam(pst, 8, model.getUrl());
+
+		// Identificador WHERE
+		setParam(pst, 9, model.getReplicationCode());
+
+		int result = pst.executeUpdate();
+		if (result > 0) {
+			connection.commit();
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public boolean delete(ReplicationModel model) throws SQLException {
+		return deleteById(model.getReplicationCode());
+	}
+
+	@Override
+	public boolean deleteById(Integer id) throws SQLException {
+		String query = getDeleteQuery(TABLE_NAME, columnId);
+		PreparedStatement pst = connection.prepareStatement(query);
+
+		setParam(pst, 1, id);
+
+		int result = pst.executeUpdate();
+		if (result > 0) {
+			connection.commit();
+
+			return true;
+		}
+
+		return false;
+	}
+	
+	@Override
+	public ReplicationModel findById(Integer id) throws SQLException {
+		ReplicationModel model = null;
+
+		String query = getFindByQuery(TABLE_NAME, columnId, "*", defaultOrderBy);
+		PreparedStatement pst = connection.prepareStatement(query);
+
+		setParam(pst, 1, id);
+		ResultSet rst = pst.executeQuery();
+
+		if (rst.next()) {
+			model = createModelFromResultSet(rst);
+		}
+
+		return model;
+	}
+	
+	@Override
+	public List<ReplicationModel> selectAll() throws SQLException {
+		String query = getSelectAllQuery(TABLE_NAME, "*", defaultOrderBy);
+
+		PreparedStatement pst = connection.prepareStatement(query);
+
+		List<ReplicationModel> replicationsList = new ArrayList<ReplicationModel>();
+
+		ResultSet rst = pst.executeQuery();
+
+		while (rst.next()) {
+			ReplicationModel model = createModelFromResultSet(rst);
+
+			replicationsList.add(model);
+		}
+
+		return replicationsList;
+	}
+	
+	/**
+	 * Cria um objeto Model a partir do resultado obtido no banco de dados
+	 * 
+	 * @param rst
+	 * @return ReplicationModel
+	 * @throws SQLException
+	 */
+	private ReplicationModel createModelFromResultSet(ResultSet rst) throws SQLException {
+		ReplicationModel model = new ReplicationModel();
+
+		model.setReplicationCode(rst.getInt("codigo_replicacao"));
+		model.setCurrentDate(rst.getDate("data_atual"));
+		model.setUser(rst.getString("usuario"));
+		model.setName(rst.getString("nome"));
+		model.setAddress(rst.getString("endereco"));
+		model.setDoor(rst.getInt("porta"));
+		model.setDatabase(rst.getString("database"));
+		model.setDatebaseType(rst.getString("tipo_banco"));
+		model.setUrl(rst.getString("url"));
+
+		return model;
+	}
+	
+}
---
+++ src/br/com/dbreplicador/model/ReplicationModel.java
@@ -0,0 +1,160 @@
+package br.com.dbreplicador.model;
+
+import java.util.Date;
+
+public class ReplicationModel  extends AbstractModel {
+	private Integer codigo_replicacao;
+	private Date data_atual;
+	private String usuario;
+	private String nome;
+	private String endereco;
+	private Integer porta;
+	private String database;
+	private String tipo_banco;
+	private String url;
+	
+	/**
+	 * codigo_replicacao
+	 */
+	public Integer getReplicationCode() {
+		return codigo_replicacao;
+	}
+	
+	/**
+	 * codigo_replicacao
+	 * 
+	 * @param replicationCode
+	 */
+	public void setReplicationCode(Integer replicationCode) {
+		this.codigo_replicacao = replicationCode;
+	}
+	
+	/**
+	 * data_atual
+	 */
+	public Date getCurrentDate() {
+		return data_atual;
+	}
+	
+	/**
+	 * data_atual
+	 * 
+	 * @param currentDate
+	 */
+	public void setCurrentDate(Date currentDate) {
+		this.data_atual = currentDate;
+	}
+	
+	/**
+	 * usuario
+	 */
+	public String getUser() {
+		return usuario;
+	}
+	
+	/**
+	 * usuario
+	 * 
+	 * @param user
+	 */
+	public void setUser(String user) {
+		this.usuario = user;
+	}
+	
+	/**
+	 * nome
+	 */
+	public String getName() {
+		return nome;
+	}
+	
+	/**
+	 * nome
+	 * 
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.nome = name;
+	}
+	
+	/**
+	 * endereco
+	 */
+	public String getAddress() {
+		return endereco;
+	}
+	
+	/**
+	 * endereco
+	 * 
+	 * @param address
+	 */
+	public void setAddress(String address) {
+		this.endereco = address;
+	}
+	
+	/**
+	 * porta
+	 */
+	public Integer getDoor() {
+		return porta;
+	}
+	
+	/**
+	 * porta
+	 * 
+	 * @param door
+	 */
+	public void setDoor(Integer door) {
+		this.porta = door;
+	}
+	
+	/**
+	 * database
+	 */
+	public String getDatabase() {
+		return database;
+	}
+	
+	/**
+	 * database
+	 * 
+	 * @param database
+	 */
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+	
+	/**
+	 * tipo_banco
+	 */
+	public String getDatebaseType() {
+		return tipo_banco;
+	}
+	
+	/**
+	 * tipo_banco
+	 * 
+	 * @param datebaseType
+	 */
+	public void setDatebaseType(String datebaseType) {
+		this.tipo_banco = datebaseType;
+	}
+	
+	/**
+	 * url
+	 */
+	public String getUrl() {
+		return url;
+	}
+	
+	/**
+	 * url
+	 * 
+	 * @param url
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}	
+	
+}
